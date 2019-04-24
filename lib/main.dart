81306d9c29e73fffa0c39950dc33e797a973d3f1
liwan11123123 <liwan11123123@qq.com>
2019-04-24 18:10:29 +0800

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';  // Add this line.
+import 'package:english_words/english_words.dart'; // Add this line.
 
 void main() => runApp(MyApp());
 
@@ -34,7 +34,7 @@ class RandomWordsState extends State<RandomWords> {
         final int index = i ~/ 2;
         print(index);
         print(_suggestions.length);
-        if(index >= _suggestions.length) {
+        if (index >= _suggestions.length) {
           print(generateWordPairs().take(10));
           _suggestions.addAll(generateWordPairs().take(10));
         }
@@ -46,10 +46,7 @@ class RandomWordsState extends State<RandomWords> {
   Widget _buildRow(WordPair pair) {
     final bool alreadySaved = _saved.contains(pair);
     return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont
-      ),
+      title: Text(pair.asPascalCase, style: _biggerFont),
       trailing: Icon(
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
@@ -66,6 +63,30 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _saved.map(
+        (WordPair pair) {
+          return ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),
+          );
+        },
+      );
+      final List<Widget> divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Saved Suggestions'),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     // final WordPair wordPair = WordPair.random();
@@ -74,6 +95,12 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushSaved,
+          )
+        ],
       ),
       body: _buildSuggestions(),
     );
