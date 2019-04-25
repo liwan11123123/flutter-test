834e9037402743021e8ee0b4da17cf19c64b2d53
liwan11123123 <liwan11123123@qq.com>
2019-04-25 11:42:51 +0800

    浮动button Scaffold小部件，点击 返回不同的Widget

--- lib/main.dart
+++ lib/main.dart
@@ -175,3 +175,69 @@
 //   ));
 // }
 
+// 浮动button Scaffold小部件，点击 返回不同的Widget
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SampleApp());
+}
+
+class SampleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sample App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SampleAppPage(),
+    );
+  }
+}
+
+class SampleAppPage extends StatefulWidget {
+  SampleAppPage({Key key}) : super(key: key);
+
+  @override
+  _SampleAppPageState createState() => _SampleAppPageState();
+}
+
+class _SampleAppPageState extends State<SampleAppPage> {
+  bool toggle = true;
+  void _toggle() {
+    setState(() {
+      toggle = !toggle;
+    });
+  }
+
+  _getToggleChild() {
+    if (toggle) {
+      return Text('Toggle One');
+    } else {
+      return MaterialButton(
+        onPressed: () {
+
+        },
+        child: Text('Toggle Two'),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sample App'),
+      ),
+      body: Center(
+        child: _getToggleChild(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toggle,
+        backgroundColor: Color.alphaBlend(Colors.green, Colors.blue),
+        tooltip: 'Update Text',
+        // child: Icon(Icons.update),
+      ),
+    );
+  }
+}
