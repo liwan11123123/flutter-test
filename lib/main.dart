5d869086a7809125f9f7af3b3441aefd1730f9b2
liwan11123123 <liwan11123123@qq.com>
2019-04-26 13:35:56 +0800

    无限滚动demo

--- lib/main.dart
+++ lib/main.dart
@@ -357,12 +357,196 @@
 //   bool shouldRepaint(SignaturePainter other) => other.points != points;
 // }
 
+// // http 请求 Demo
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
 
+// void main() {
+//   runApp(SampleApp());
+// }
 
-// http 请求 Demo
-import 'dart:convert';
+// class SampleApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Sample App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: SampleAppPage(),
+//     );
+//   }
+// }
+
+// class SampleAppPage extends StatefulWidget {
+//   SampleAppPage({Key key}) : super(key: key);
+
+//   @override
+//   _SampleAppPageState createState() => _SampleAppPageState();
+// }
+
+// class _SampleAppPageState extends State<SampleAppPage> {
+//   List widgets = [];
+
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     loadData();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Sample App'),
+//       ),
+//       body: ListView.builder(
+//         itemCount: widgets.length,
+//         itemBuilder: (BuildContext context, int position) {
+//           return getRow(position);
+//         },
+//       ),
+//     );
+//   }
+
+//   Widget getRow(int i) {
+//     return Padding(
+//       padding: EdgeInsets.all(10.0),
+//       child: Text('Row ${widgets[i]["title"]}'),
+//     );
+//   }
+
+//   loadData() async {
+//     String dataURL = "https://jsonplaceholder.typicode.com/posts";
+//     http.Response response = await http.get(dataURL, headers: {'cookies': '111'});
+//     setState(() {
+//       widgets = json.decode(response.body);
+//     });
+//   }
+// }
+
+// import 'dart:convert';
+
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+// import 'dart:async';
+// import 'dart:isolate';
+
+// void main() {
+//   runApp(SampleApp());
+// }
+
+// class SampleApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Sample App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: SampleAppPage(),
+//     );
+//   }
+// }
+
+// class SampleAppPage extends StatefulWidget {
+//   SampleAppPage({Key key}) : super(key: key);
+
+//   @override
+//   _SampleAppPageState createState() => _SampleAppPageState();
+// }
+
+// class _SampleAppPageState extends State<SampleAppPage> {
+//   List widgets = [];
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     loadData();
+//   }
+
+//   showLoadingDialog() {
+//     if (widgets.length == 0) {
+//       return true;
+//     }
+
+//     return false;
+//   }
+
+//   getBody() {
+//     if (showLoadingDialog()) {
+//       return getProgressDialog();
+//     } else {
+//       return getListView();
+//     }
+//   }
+
+//   getProgressDialog() {
+//     return Center(child: CircularProgressIndicator(),);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Sample App'),
+//       ),
+//       body: getBody(),
+//     );
+//   }
+
+//   ListView getListView() => ListView.builder(
+//     itemCount: widgets.length,
+//     itemBuilder: (BuildContext context, int position) {
+//       return getRow(position);
+//     },
+//   );
+
+//   Widget getRow(int i) {
+//     return Padding(padding: EdgeInsets.all(10.0), child: Text('Row ${widgets[i]["title"]}'),);
+//   }
+
+//   loadData() async {
+//     ReceivePort receivePort = ReceivePort();
+//     await Isolate.spawn(dataLoader, receivePort.sendPort);
+
+//     SendPort sendPort = await receivePort.first;
+
+//     List msg = await sendReceive(sendPort, "https://jsonplaceholder.typicode.com/posts");
+
+//     setState(() {
+//       widgets = msg;
+//     });
+//   }
+
+//   static dataLoader(SendPort sendPort) async {
+//     ReceivePort port = ReceivePort();
+
+//     sendPort.send(port.sendPort);
+
+//     await for (var msg in port) {
+//       String data = msg[0];
+//       SendPort replyTo = msg[1];
+
+//       String dataURL = data;
+//       http.Response response = await http.get(dataURL);
+
+//       replyTo.send(json.decode(response.body));
+//     }
+//   }
+
+//   Future sendReceive(SendPort port, msg) {
+//     ReceivePort response = ReceivePort();
+//     port.send([msg, response.sendPort]);
+//     return response.first;
+//   }
+
+// }
+
+// 无限滚动 demo
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 void main() {
   runApp(SampleApp());
@@ -389,42 +573,64 @@ class SampleAppPage extends StatefulWidget {
 }
 
 class _SampleAppPageState extends State<SampleAppPage> {
-  List widgets = [];
+  List widgets = <Widget>[];
 
   @override
   void initState() {
     super.initState();
-
-    loadData();
+    for (var i = 0; i < 100; i++) {
+      widgets.add(getRow(i));
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sample App'),
+        title: TextField(
+          decoration: InputDecoration(hintText: "This is a hint"),
+        ),
       ),
       body: ListView.builder(
         itemCount: widgets.length,
         itemBuilder: (BuildContext context, int position) {
           return getRow(position);
-        },
-      ),
-    );
+        }),
+      );
   }
 
   Widget getRow(int i) {
-    return Padding(
-      padding: EdgeInsets.all(10.0),
-      child: Text('Row ${widgets[i]["title"]}'),
+    return GestureDetector(
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Text('Row $i'),
+      ),
+      onTap: () {
+        // setState(() {
+        //   widgets.add(getRow(widgets.length + 1));
+        //   print('rot $i');
+        // });
+        print('row $i');
+      },
     );
   }
-
-  loadData() async {
-    String dataURL = "https://jsonplaceholder.typicode.com/posts";
-    http.Response response = await http.get(dataURL, headers: {'cookies': '111'});
-    setState(() {
-      widgets = json.decode(response.body);
-    });
-  }
+  // _getListData() {
+  //   List<Widget> widgets = [];
+  //   for (int i = 0; i < 100; i++) {
+  //     widgets.add(GestureDetector(
+  //       child: Padding(
+  //         padding: EdgeInsets.all(10.0),
+  //         child: Text('Row $i'),
+  //       ),
+  //       onTap: () {
+  //         print('row tapped');
+  //       },
+  //     ));
+  //     // widgets.add(Padding(
+  //     //   padding: EdgeInsets.all(10.0),
+  //     //   child: Text('Row $i'),
+  //     // ));
+  //   }
+  //   return widgets;
+  // }
 }
