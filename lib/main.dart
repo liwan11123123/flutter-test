d70c18e49bd28b254d9c598ba0b4d58c0a0d969c
liwan11123123 <liwan11123123@qq.com>
2019-04-27 11:05:58 +0800

    加载数据

--- lib/main.dart
+++ lib/main.dart
@@ -545,7 +545,7 @@
 
 // }
 
-// 无限滚动 
+// 无限滚动
 // import 'package:flutter/material.dart';
 
 // void main() {
@@ -616,9 +616,354 @@
 //   }
 // }
 
-
 // 输入框 校验email
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(SampleApp());
+// }
+
+// class SampleApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Sample App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: SampleAppPage(),
+//     );
+//   }
+// }
+
+// class SampleAppPage extends StatefulWidget {
+//   SampleAppPage({Key key}) : super(key: key);
+
+//   @override
+//   _SampleAppPageState createState() => _SampleAppPageState();
+// }
+
+// class _SampleAppPageState extends State<SampleAppPage> {
+//   String _errorText;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Sample App'),
+//       ),
+//       body: Center(
+//         child: TextField(
+//           onSubmitted: (String text) {
+//             setState(() {
+//               if (!isEmail(text)) {
+//                 _errorText = 'Error: this is not an email';
+//               } else {
+//                 _errorText = null;
+//               }
+//             });
+//           },
+//           decoration: InputDecoration(hintText: 'This is a hit.', errorText: _getErrorText()),
+//         ),
+//       )
+//       );
+//   }
+
+//   _getErrorText() {
+//     return _errorText;
+//   }
+
+//   bool isEmail(String em) {
+//     String emailRegexp =
+//       r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
+//     RegExp regExp = RegExp(emailRegexp);
+
+//     return regExp.hasMatch(em);
+//   }
+// }
+
+// class SampleApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Sample App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: SampleAppPage(),
+//     );
+//   }
+// }
+
+// class SampleAppPage extends StatefulWidget {
+//   SampleAppPage({Key key}) : super(key: key);
+
+//   @override
+//   _SampleAppPageState createState() => _SampleAppPageState();
+// }
+
+// class _SampleAppPageState extends State<SampleAppPage> {
+//   // Default value for toggle
+//   bool toggle = true;
+//   void _toggle() {
+//     setState(() {
+//       toggle = !toggle;
+//     });
+//   }
+
+//   _getToggleChild() {
+//     if (toggle) {
+//       return Text('Toggle One');
+//     } else {
+//       return Text('toggle two');
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Sample App"),
+//       ),
+//       body: Center(
+//         child: _getToggleChild(),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _toggle,
+//         tooltip: 'Update Text',
+//         child: Icon(Icons.update),
+//       ),
+//     );
+//   }
+// }
+
+// import 'package:flutter/material.dart';
+// import 'package:url_launcher/url_launcher.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: '111',
+//       home: Scaffold(
+//         body: Center(
+//           child: RaisedButton(
+//             onPressed: _launchURL,
+//             child: Text('Show Flutter homepage'),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// _launchURL() async {
+//   const url = 'https://flutter.io';
+//   if (await canLaunch(url)) {
+//     await launch(url);
+//   } else {
+//     throw 'Could not launch $url';
+//   }
+// }
+
+
+// import 'dart:convert';
+
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+
+// void main() {
+//   runApp(SampleApp());
+// }
+
+// class SampleApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Sample App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: SampleAppPage(),
+//     );
+//   }
+// }
+
+// class SampleAppPage extends StatefulWidget {
+//   SampleAppPage({Key key}) : super(key: key);
+
+//   @override
+//   _SampleAppPageState createState() => _SampleAppPageState();
+// }
+
+// class _SampleAppPageState extends State<SampleAppPage> {
+//   List widgets = [];
+
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     loadData();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Sample App"),
+//       ),
+//       body: ListView.builder(
+//           itemCount: widgets.length,
+//           itemBuilder: (BuildContext context, int position) {
+//             return getRow(position);
+//           }));
+//   }
+
+//   Widget getRow(int i) {
+//     return Padding(
+//       padding: EdgeInsets.all(10.0),
+//       child: Text("Row ${widgets[i]["title"]}")
+//     );
+//   }
+
+//   loadData() async {
+//     String dataURL = "https://jsonplaceholder.typicode.com/posts";
+//     http.Response response = await http.get(dataURL);
+//     setState(() {
+//       widgets = json.decode(response.body);
+//     });
+//   }
+// }
+
+// import 'dart:convert';
+
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+// import 'dart:async';
+// import 'dart:isolate';
+
+// void main() {
+//   runApp(SampleApp());
+// }
+
+// class SampleApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Sample App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: SampleAppPage(),
+//     );
+//   }
+// }
+
+// class SampleAppPage extends StatefulWidget {
+//   SampleAppPage({Key key}) : super(key: key);
+
+//   @override
+//   _SampleAppPageState createState() => _SampleAppPageState();
+// }
+
+// class _SampleAppPageState extends State<SampleAppPage> {
+//   List widgets = [];
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     loadData();
+//   }
+
+//   showLoadingDialog() {
+//     if (widgets.length == 0) {
+//       return true;
+//     }
+
+//     return false;
+//   }
+
+//   getBody() {
+//     if (showLoadingDialog()) {
+//       return getProgressDialog();
+//     } else {
+//       return getListView();
+//     }
+//   }
+
+//   getProgressDialog() {
+//     return Center(child: CircularProgressIndicator());
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: Text("Sample App"),
+//         ),
+//         body: getBody());
+//   }
+
+//   ListView getListView() => ListView.builder(
+//       itemCount: widgets.length,
+//       itemBuilder: (BuildContext context, int position) {
+//         return getRow(position);
+//       });
+
+//   Widget getRow(int i) {
+//     return Padding(padding: EdgeInsets.all(10.0), child: Text("Row ${widgets[i]["title"]}"));
+//   }
+
+//   loadData() async {
+//     ReceivePort receivePort = ReceivePort();
+//     await Isolate.spawn(dataLoader, receivePort.sendPort);
+
+//     // The 'echo' isolate sends its SendPort as the first message
+//     SendPort sendPort = await receivePort.first;
+
+//     List msg = await sendReceive(sendPort, "https://jsonplaceholder.typicode.com/posts");
+
+//     setState(() {
+//       widgets = msg;
+//     });
+//   }
+
+// // the entry point for the isolate
+//   static dataLoader(SendPort sendPort) async {
+//     // Open the ReceivePort for incoming messages.
+//     ReceivePort port = ReceivePort();
+
+//     // Notify any other isolates what port this isolate listens to.
+//     sendPort.send(port.sendPort);
+
+//     await for (var msg in port) {
+//       String data = msg[0];
+//       SendPort replyTo = msg[1];
+
+//       String dataURL = data;
+//       http.Response response = await http.get(dataURL);
+//       // Lots of JSON to parse
+//       replyTo.send(json.decode(response.body));
+//     }
+//   }
+
+//   Future sendReceive(SendPort port, msg) {
+//     ReceivePort response = ReceivePort();
+//     port.send([msg, response.sendPort]);
+//     return response.first;
+//   }
+// }
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(SampleApp());
@@ -645,41 +990,54 @@ class SampleAppPage extends StatefulWidget {
 }
 
 class _SampleAppPageState extends State<SampleAppPage> {
-  String _errorText;
+  List widgets = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  showLoadingDialog() {
+    return widgets.length == 0;
+  }
+
+  getBody() {
+    if (showLoadingDialog()) {
+      return getProgressDialog();
+    } else {
+      return getListView();
+    }
+  }
+
+  getProgressDialog() {
+    return Center(child: CircularProgressIndicator());
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Sample App'),
-      ),
-      body: Center(
-        child: TextField(
-          onSubmitted: (String text) {
-            setState(() {
-              if (!isEmail(text)) {
-                _errorText = 'Error: this is not an email';
-              } else {
-                _errorText = null;
-              }
-            });
-          },
-          decoration: InputDecoration(hintText: 'This is a hit.', errorText: _getErrorText()),
+        appBar: AppBar(
+          title: Text("Sample App"),
         ),
-      )
-      );
+        body: getBody());
   }
 
-  _getErrorText() {
-    return _errorText;
+  ListView getListView() => ListView.builder(
+      itemCount: widgets.length,
+      itemBuilder: (BuildContext context, int position) {
+        return getRow(position);
+      });
+
+  Widget getRow(int i) {
+    return Padding(padding: EdgeInsets.all(10.0), child: Text("Row ${widgets[i]["title"]}"));
   }
 
-  bool isEmail(String em) {
-    String emailRegexp = 
-      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-
-    RegExp regExp = RegExp(emailRegexp);
-
-    return regExp.hasMatch(em);
+  loadData() async {
+    String dataURL = "https://jsonplaceholder.typicode.com/posts";
+    http.Response response = await http.get(dataURL);
+    setState(() {
+      widgets = json.decode(response.body);
+    });
   }
 }
