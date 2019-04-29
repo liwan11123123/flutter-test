2ae9f32215fb0f7a46b370dc922ecc0d70f71448
liwan11123123 <liwan11123123@qq.com>
2019-04-29 15:16:22 +0800

    通过Image.network 加载网络图像

--- lib/main.dart
+++ lib/main.dart
@@ -1929,64 +1929,89 @@
 // }
 
 // 滑动删除list 中的一项
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   MyApp({Key key}) : super(key: key);
+
+//   @override
+//   MyAppState createState() {
+//     return MyAppState();
+//   }
+// }
+
+// class MyAppState extends State<MyApp> {
+//   final items = List<String>.generate(3, (i) => 'Item ${i + 1}');
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final title = 'Dismissing Items';
+
+//     return MaterialApp(
+//       title: title,
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text(title),
+//         ),
+//         body: ListView.builder(
+//           itemCount: items.length,
+//           itemBuilder: (BuildContext context, i) {
+//             final item = items[i];
+
+//             return Dismissible(
+//               key: Key(item),
+//               onDismissed: (direction) {
+//                 setState(() {
+//                   items.removeAt(i);
+//                 });
+
+//                 Scaffold.of(context).showSnackBar(
+//                   SnackBar(
+//                     content: Text('$item dismissed'),
+//                   ),
+//                 );
+//               },
+//               background: Container(
+//                 color: Colors.red,
+//               ),
+//               child: ListTile(
+//                 title: Text('$item'),
+//               ),
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+// 通过Image.network 加载网络图像
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  MyApp({Key key}) : super(key: key);
-
-  @override
-  MyAppState createState() {
-    return MyAppState();
-  }
-}
-
-class MyAppState extends State<MyApp> {
-  final items = List<String>.generate(3, (i) => 'Item ${i + 1}');
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final title = 'Dismissing Items';
+    var title = 'Web Images';
 
     return MaterialApp(
       title: title,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text(title),
         ),
-        body: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (BuildContext context, i) {
-            final item = items[i];
-
-            return Dismissible(
-              key: Key(item),
-              onDismissed: (direction) {
-                setState(() {
-                  items.removeAt(i);
-                });
-
-                Scaffold.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('$item dismissed'),
-                  ),
-                );
-              },
-              background: Container(
-                color: Colors.red,
-              ),
-              child: ListTile(
-                title: Text('$item'),
-              ),
-            );
-          },
+        body: Image.network(
+          'https://picsum.photos/250?image=9',
         ),
       ),
     );
