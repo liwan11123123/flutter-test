543a281141a21066b98a2a7a409698a17d76e895
liwan11123123 <liwan11123123@qq.com>
2019-04-29 19:12:03 +0800

    根据值，返回不同的 Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2179,18 +2179,66 @@
 //   }
 // }
 
-
 // GridView 网格
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final title = 'Grid List';
+
+//     return MaterialApp(
+//       title: title,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text(title),
+//         ),
+//         body: GridView.count(
+//           // Create a grid with 2 columns. If you change the scrollDirection to
+//           // horizontal, this would produce 2 rows.
+//           crossAxisCount: 2,
+//           // Generate 100 Widgets that display their index in the List
+//           children: List.generate(100, (index) {
+//             return Center(
+//               child: Text(
+//                 'Item $index',
+//                 style: Theme.of(context).textTheme.headline,
+//               ),
+//             );
+//           }),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// 根据值，返回不同的 Widget
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp(
+    items: List<ListItem>.generate(
+      1000,
+      (i) => i % 6 == 0
+          ? HeadingItem('Heading $i')
+          : MessageItem('Sender $i', 'Message body $i'),
+    ),
+  ));
 }
 
 class MyApp extends StatelessWidget {
+  final List<ListItem> items;
+
+  MyApp({Key key, @required this.items}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    final title = 'Grid List';
+    final title = 'Mixed List';
 
     return MaterialApp(
       title: title,
@@ -2198,21 +2246,42 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text(title),
         ),
-        body: GridView.count(
-          // Create a grid with 2 columns. If you change the scrollDirection to
-          // horizontal, this would produce 2 rows.
-          crossAxisCount: 2,
-          // Generate 100 Widgets that display their index in the List
-          children: List.generate(100, (index) {
-            return Center(
-              child: Text(
-                'Item $index',
-                style: Theme.of(context).textTheme.headline,
-              ),
-            );
-          }),
-        ),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+
+            if (item is HeadingItem) {
+              return ListTile(
+                title: Text(
+                  item.heading,
+                  style: Theme.of(context).textTheme.headline,
+                ),
+              );
+            } else if (item is MessageItem) {
+              return ListTile(
+                title: Text(item.sender),
+                subtitle: Text(item.body),
+              );
+            }
+          },
+        )
       ),
     );
   }
+}
+
+abstract class ListItem {}
+
+class HeadingItem implements ListItem {
+  final String heading;
+
+  HeadingItem(this.heading);
+}
+
+class MessageItem implements ListItem {
+  final String sender;
+  final String body;
+
+  MessageItem(this.sender, this.body);
 }
