ba4d9846017ac718230237b24ba4989fdeba0271
liwan11123123 <liwan11123123@qq.com>
2019-04-29 12:06:07 +0800

    Dialog model弹出层

--- lib/main.dart
+++ lib/main.dart
@@ -1692,6 +1692,76 @@
 
 
 // 两种方式监听输入框，一种 onChange， 另一种 TextEditingController
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Retrieve Text Input',
+//       home: MyCustomForm(),
+//     );
+//   }
+// }
+
+// // Define a Custom Form Widget
+// class MyCustomForm extends StatefulWidget {
+//   @override
+//   _MyCustomFormState createState() => _MyCustomFormState();
+// }
+
+// // Define a corresponding State class. This class will hold the data related to
+// // our Form.
+// class _MyCustomFormState extends State<MyCustomForm> {
+//   final myController = TextEditingController();
+
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     myController.addListener(_printLatestValue);
+//   }
+
+//   @override
+//   void dispose() {
+//     myController.dispose();
+//     super.dispose();
+//   }
+
+//   _printLatestValue() {
+//     print('Second text field: ${myController.value}');
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Retrieve Text Input'),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           children: <Widget>[
+//             TextField(
+//               onChanged: (text) {
+//                 print('First text field: $text');
+//               },
+//               maxLength: 11,
+//             ),
+//             TextField(
+//               controller: myController,
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+// Dialog model弹出层
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -1718,20 +1788,9 @@ class _MyCustomFormState extends State<MyCustomForm> {
   final myController = TextEditingController();
 
   @override
-  void initState() {
-    super.initState();
-
-    myController.addListener(_printLatestValue);
-  }
-
-  @override
   void dispose() {
     myController.dispose();
     super.dispose();
-  }
-
-  _printLatestValue() {
-    print('Second text field: ${myController.value}');
   }
 
   @override
@@ -1742,19 +1801,23 @@ class _MyCustomFormState extends State<MyCustomForm> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              onChanged: (text) {
-                print('First text field: $text');
-              },
-              maxLength: 11,
-            ),
-            TextField(
-              controller: myController,
-            )
-          ],
+        child: TextField(
+          controller: myController,
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          return showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                content: Text(myController.text),
+              );
+            }
+          );
+        },
+        tooltip: 'Show me the value!',
+        child: Icon(Icons.text_fields),
       ),
     );
   }
