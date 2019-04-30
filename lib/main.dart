c42d6fa39149b55c7a7b98b398fa7af3f9a21cd2
liwan11123123 <liwan11123123@qq.com>
2019-04-30 12:49:22 +0800

    无限滚动列表

--- lib/main.dart
+++ lib/main.dart
@@ -2288,36 +2288,75 @@
 
 
 // 在列表上方放置一个浮动应用栏
+// import 'package:flutter/foundation.dart';
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   MyApp({Key key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final title = 'Floating App Bar';
+
+//     return MaterialApp(
+//       title: title,
+//       home: Scaffold(
+//         body: CustomScrollView(
+//           slivers: <Widget>[
+//             SliverAppBar(
+//               title: Text('title'),
+//               floating: false,
+//               flexibleSpace: Text('data'),
+//               expandedHeight: 200,
+//             ),
+//             SliverList(
+//               delegate: SliverChildBuilderDelegate(
+//                 (context, index) => ListTile(title: Text('Item $index'),),
+//                 childCount: 1000,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+// 无限滚动列表
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp(
+    items: List<String>.generate(10000, (i) => 'Item $i'),
+  ));
+}
 
 class MyApp extends StatelessWidget {
-  MyApp({Key key}) : super(key: key);
+  final List<String> items;
+
+  MyApp({Key key, @required this.items}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final title = 'Floating App Bar';
+    final title = 'Long List';
 
     return MaterialApp(
       title: title,
       home: Scaffold(
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              title: Text('title'),
-              floating: false,
-              flexibleSpace: Text('data'),
-              expandedHeight: 200,
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) => ListTile(title: Text('Item $index'),),
-                childCount: 1000,
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text('${items[index]}'),
+            );
+          },
         ),
       ),
     );
