d47559252ae8f797267b7f5da8319dc4e6b87b00
liwan11123123 <liwan11123123@qq.com>
2019-05-02 18:40:34 +0800

    从互联网上获取数据

--- lib/main.dart
+++ lib/main.dart
@@ -2837,20 +2837,97 @@
 
 
 // 从互联网上获取数据
+// import 'dart:async';
+// import 'dart:convert';
+
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+
+// const url = 'https://jsonplaceholder.typicode.com/posts/1';
+
+// Future<Post> fetchPost() async {
+//   final response = await http.get(url);
+
+//   if (response.statusCode == 200) {
+//     return Post.fromJson(json.decode(response.body));
+//   } else {
+//     throw Exception('Failed to load post');
+//   }
+// }
+
+// class Post {
+//   final int userId;
+//   final int id;
+//   final String title;
+//   final String body;
+
+//   Post({this.userId, this.id, this.title, this.body});
+
+//   factory Post.fromJson(Map<String, dynamic> json) {
+//     return Post(
+//       userId: json['userId'],
+//       id: json['id'],
+//       title: json['title'],
+//       body: json['body'],
+//     );
+//   }
+// }
+
+// void main() => runApp(MyApp(post: fetchPost()));
+
+// class MyApp extends StatelessWidget {
+//   final Future<Post> post;
+
+//   MyApp({Key key, this.post}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Fetch Data Example',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Fetch Data Example'),
+//         ),
+//         body: Center(
+//           child: FutureBuilder<Post>(
+//             future: post,
+//             builder: (context, snapshot) {
+//               print(snapshot);
+//               if (snapshot.hasData) {
+//                 return Text(snapshot.data.title);
+//               } else if (snapshot.hasError) {
+//                 return Text('${snapshot.error}');
+//               }
+
+//               return CircularProgressIndicator();
+//             },
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+// 从互联网上获取数据
 import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-const url = 'https://jsonplaceholder.typicode.com/posts/1';
-
 Future<Post> fetchPost() async {
-  final response = await http.get(url);
+  final response =
+      await http.get('https://jsonplaceholder.typicode.com/posts/1');
 
   if (response.statusCode == 200) {
+    // If the call to the server was successful, parse the JSON
     return Post.fromJson(json.decode(response.body));
   } else {
+    // If that call was not successful, throw an error.
     throw Exception('Failed to load post');
   }
 }
@@ -2895,7 +2972,6 @@ class MyApp extends StatelessWidget {
           child: FutureBuilder<Post>(
             future: post,
             builder: (context, snapshot) {
-              print(snapshot);
               if (snapshot.hasData) {
                 return Text(snapshot.data.title);
               } else if (snapshot.hasError) {
