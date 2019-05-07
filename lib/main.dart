3cb31fbe58e5d3157579bab8b8e60839aa48940d
liwan11123123 <liwan11123123@qq.com>
2019-05-07 11:26:41 +0800

    使用sqflite 本地数据库

--- lib/main.dart
+++ lib/main.dart
@@ -3105,87 +3105,201 @@
 
 
 // 使用WebSockets
-import 'package:flutter/foundation.dart';
-import 'package:web_socket_channel/io.dart';
-import 'package:flutter/material.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
+// import 'package:flutter/foundation.dart';
+// import 'package:web_socket_channel/io.dart';
+// import 'package:flutter/material.dart';
+// import 'package:web_socket_channel/web_socket_channel.dart';
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final title = 'WebSocket Demo';
-    return MaterialApp(
-      title: title,
-      home: MyHomePage(
-        title: title,
-        channel: IOWebSocketChannel.connect('ws://echo.websocket.org'),
-      ),
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final title = 'WebSocket Demo';
+//     return MaterialApp(
+//       title: title,
+//       home: MyHomePage(
+//         title: title,
+//         channel: IOWebSocketChannel.connect('ws://echo.websocket.org'),
+//       ),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   final String title;
+//   final WebSocketChannel channel;
+
+//   MyHomePage({Key key, @required this.title, @required this.channel})
+//     : super(key: key);
+
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   TextEditingController _controller = TextEditingController();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(20.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: <Widget>[
+//             Form(
+//               child: TextFormField(
+//                 controller: _controller,
+//                 decoration: InputDecoration(labelText: 'Send a message'),
+//               ),
+//             ),
+//             StreamBuilder(
+//               stream: widget.channel.stream,
+//               builder: (context, snapshot) {
+//                 return Padding(
+//                   padding: const EdgeInsets.symmetric(vertical: 24.0),
+//                   child: Text(snapshot.hasData ? '${snapshot.data}' : ''),
+//                 );
+//               },
+//             )
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _sendMessage,
+//         tooltip: '发送消息',
+//         child: Icon(Icons.send),
+//       ),
+//     );
+//   }
+
+//   void _sendMessage() {
+//     if (_controller.text.isNotEmpty) {
+//       widget.channel.sink.add(_controller.text);
+//     }
+//   }
+
+//   @override
+//   void dispose() {
+//     widget.channel.sink.close();
+//     super.dispose();
+//   }
+// }
+
+
+// 使用sqflite 本地数据库
+import 'dart:async';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+void main() async {
+  final database = openDatabase(
+    join(await getDatabasesPath(), 'doggle_database.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)',
+      );
+    },
+    version: 1,
+  );
+
+  Future<void> insertDog(Dog dog) async {
+    final Database db = await database;
+
+    await db.insert(
+      'dogs',
+      dog.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  final String title;
-  final WebSocketChannel channel;
+  Future<List<Dog>> dogs() async {
+    final Database db = await database;
 
-  MyHomePage({Key key, @required this.title, @required this.channel})
-    : super(key: key);
+    final List<Map<String, dynamic>> maps = await db.query('dogs');
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+    return List.generate(maps.length, (i) {
+      return Dog(
+        id: maps[i]['id'],
+        name: maps[i]['name'],
+        age: maps[i]['age'],
+      );
+    });
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController _controller = TextEditingController();
+  Future<void> updateDog(Dog dog) async {
+    final db = await database;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Form(
-              child: TextFormField(
-                controller: _controller,
-                decoration: InputDecoration(labelText: 'Send a message'),
-              ),
-            ),
-            StreamBuilder(
-              stream: widget.channel.stream,
-              builder: (context, snapshot) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 24.0),
-                  child: Text(snapshot.hasData ? '${snapshot.data}' : ''),
-                );
-              },
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _sendMessage,
-        tooltip: '发送消息',
-        child: Icon(Icons.send),
-      ),
+    await db.update(
+      'dogs',
+      dog.toMap(),
+      where: 'id = ?',
+      whereArgs: [dog.id],
     );
   }
 
-  void _sendMessage() {
-    if (_controller.text.isNotEmpty) {
-      widget.channel.sink.add(_controller.text);
-    }
+  Future<void> deleteDog(int id) async {
+    final db = await database;
+
+    await db.delete(
+      'dogs',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  var fido = Dog(
+    id: 0,
+    name: 'Fido',
+    age: 35,
+  );
+
+  await insertDog(fido);
+  var fido1 = Dog(
+    id: 1,
+    name: 'F22ido',
+    age: 353,
+  );
+  await insertDog(fido1);
+
+  print(await dogs());
+
+  fido = Dog(
+    id: fido.id,
+    name: fido.name,
+    age: fido.age + 11,
+  );
+  await updateDog(fido);
+
+  print(await dogs());
+
+  await deleteDog(fido.id);
+
+  print(await dogs());
+}
+
+class Dog {
+  final int id;
+  final String name;
+  final int age;
+
+  Dog({this.id, this.name, this.age});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+    };
   }
 
   @override
-  void dispose() {
-    widget.channel.sink.close();
-    super.dispose();
+  String toString() {
+    return 'Dog{id: $id, name: $name, age: $age}';
   }
 }
